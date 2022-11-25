73353a0ded27f1f0bf13883823941b3947bb6ee7
Ali <sabirali2560@gmail.com>
2022-11-25 17:36:36 -0600

    Added support for multiline syscalls

--- main.py
+++ main.py
@@ -20,9 +20,30 @@ def get_fn_arguments(syscall, fn_name):
     fn_args_list = [i.strip() for i in fn_args_str.split(",")]
     return fn_args_list
 
+def get_fields_for_unfinished(syscall):
+    syscall = syscall.replace("\n", "")
+    res = syscall.split(" ")
+    return res[0], res[1]
+
+def parse_call_for_unfinished(syscall, fn_name):
+    pid, timestamp = get_fields_for_unfinished(syscall)
+    fn_args_list = get_fn_arguments_for_unfinished(syscall, fn_name)
+    return [pid, timestamp] + fn_args_list
+
+def get_fn_arguments_for_unfinished(syscall, fn_name):
+    pattern = re.compile("(?<=" + fn_name + "\().*(?=\<)")
+    fn_args_str = pattern.findall(syscall)[0]
+    fn_args_list = [i.strip() for i in fn_args_str.split(",")]
+    return fn_args_list
+
+def get_fields_for_resumed(syscall):
+    syscall = syscall.replace("\n", "")
+    res = syscall.split(" ")
+    return res[0], res[-1]    
+
 
 if __name__ == "__main__":
-    strace_file = open("strace.txt", "r")
+    strace_file = open("strace2.txt", "r")
 
     syscall_to_args_map = {
         "mmap": ["addr", "length", "prot", "flags", "fd", "offset"],
@@ -36,11 +57,24 @@ if __name__ == "__main__":
     metadata_headers = ["pid", "timestamp", "duration"]
     syscall_to_args_map = {key: metadata_headers + syscall_to_args_map[key] for key in syscall_to_args_map}
     syscall_results_map = {key: [] for key in syscall_to_args_map}
+    syscall_to_stacks_map = dict()
 
     for line in strace_file.readlines():
         for call_to_parse in syscall_to_args_map:
             if " " + call_to_parse + "(" in line:
-                syscall_results_map[call_to_parse].append(parse_call(line, call_to_parse))
+                if "<unfinished ...>" not in line:
+                    syscall_results_map[call_to_parse].append(parse_call(line, call_to_parse))
+                else:
+                    arguments = parse_call_for_unfinished(line, call_to_parse)
+                    if arguments[0] + call_to_parse not in syscall_to_stacks_map:
+                        syscall_to_stacks_map[arguments[0] + call_to_parse] = []
+                    syscall_to_stacks_map[arguments[0] + call_to_parse].append(arguments)
+                break    
+            elif "<... " + call_to_parse + " resumed>" in line:
+                fields = get_fields_for_resumed(line)
+                arguments = syscall_to_stacks_map[fields[0] + call_to_parse].pop()
+                arguments.insert(2, fields[-1])
+                syscall_results_map[call_to_parse].append(arguments)    
                 break
 
     # Save results of each call in their own csv
