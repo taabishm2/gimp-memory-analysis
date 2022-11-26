0ae19e6267319750426560c33879b5d7d960904f
Ali <sabirali2560@gmail.com>
2022-11-25 18:02:41 -0600

    added return value

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import re
 import csv
 
 
-def get_fields(syscall):
+def get_fields(syscall, fn_name):
     syscall = syscall.replace("\n", "")
     res = syscall.split(" ")
-    return res[0], res[1], res[-1]
+    return res[0], res[1], res[-2], res[-1]
 
 
 def parse_call(syscall, fn_name):
-    pid, timestamp, duration = get_fields(syscall)
+    pid, timestamp, ret_val, duration = get_fields(syscall, fn_name)
     fn_args_list = get_fn_arguments(syscall, fn_name)
-    return [pid, timestamp, duration] + fn_args_list
+    return [pid, timestamp, ret_val, duration] + fn_args_list
 
 
 def get_fn_arguments(syscall, fn_name):
@@ -39,7 +39,7 @@ def get_fn_arguments_for_unfinished(syscall, fn_name):
 def get_fields_for_resumed(syscall):
     syscall = syscall.replace("\n", "")
     res = syscall.split(" ")
-    return res[0], res[-1]    
+    return res[0], res[-2], res[-1]    
 
 
 if __name__ == "__main__":
@@ -54,7 +54,7 @@ if __name__ == "__main__":
         "shmat" : ["shmid", "shmaddr", "shmflg"],
     }
 
-    metadata_headers = ["pid", "timestamp", "duration"]
+    metadata_headers = ["pid", "timestamp", "ret_val", "duration"]
     syscall_to_args_map = {key: metadata_headers + syscall_to_args_map[key] for key in syscall_to_args_map}
     syscall_results_map = {key: [] for key in syscall_to_args_map}
     syscall_to_stacks_map = dict()
@@ -73,7 +73,8 @@ if __name__ == "__main__":
             elif "<... " + call_to_parse + " resumed>" in line:
                 fields = get_fields_for_resumed(line)
                 arguments = syscall_to_stacks_map[fields[0] + call_to_parse].pop()
-                arguments.insert(2, fields[-1])
+                arguments.insert(2, fields[-2])
+                arguments.insert(3, fields[-1])
                 syscall_results_map[call_to_parse].append(arguments)    
                 break
 
