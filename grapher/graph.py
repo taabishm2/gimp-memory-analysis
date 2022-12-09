e37a39074cfab7a4171037c8aa8b28ff95143b78
tabish <tabish@wisc.edu>
2022-12-09 02:20:13 -0600

    Add collection and plotting function for memory-histograms

--- grapher/graph.py
+++ grapher/graph.py
@@ -5,6 +5,8 @@ import subprocess
 import time
 import csv
 import os
+import re
+import math
 
 
 class GraphName(Enum):
@@ -58,6 +60,60 @@ class Graph:
             print("# PLOTTING MEMUSE FOR", gimp_test.name)
             plt.clf()
             self.plot_memory_consumed(gimp_test)
+
+            print("# PLOTTING MEM-CNT FOR", gimp_test.name)
+            plt.clf()
+            self.plot_memory_memory_count(gimp_test)
+
+            print("# PLOTTING MEM-MEM FOR", gimp_test.name)
+            plt.clf()
+            self.plot_memory_memory_size(gimp_test)
+
+    def plot_memory_memory_size(self, gimp_test: GimpTestName):
+        mmap_file = open("input/" + self.allocator.name + "-" + gimp_test.name + "-mmap-parsed.csv")
+        munmap_file = open("input/" + self.allocator.name + "-" + gimp_test.name + "-munmap-parsed.csv")
+        brk_file = open("input/" + self.allocator.name + "-" + gimp_test.name + "-brk-parsed.csv")
+
+        mmap_reader = csv.reader(mmap_file)
+        munmap_reader = csv.reader(munmap_file)
+        brk_reader = csv.reader(brk_file)
+        next(mmap_reader)
+        next(munmap_reader)
+        next(brk_reader)
+
+        bins = 100
+        mmap_sizes = [math.log(int(i[5])) for i in mmap_reader]
+        hist = [0] * bins
+        interval_size = math.ceil(max(mmap_sizes) / bins)
+        for i in mmap_sizes:
+            hist[int(i // interval_size)] += i
+
+        plt.bar([i for i in range(bins)], hist)
+
+        plt.ylabel('Memory')
+        plt.xlabel('Log(Memory)')
+        plt.savefig(
+            "output/" + self.allocator.name + "-" + gimp_test.name + "-" + GraphName.STRACE_MEMORY_MEMORY_HIST.name)
+
+    def plot_memory_memory_count(self, gimp_test: GimpTestName):
+        mmap_file = open("input/" + self.allocator.name + "-" + gimp_test.name + "-mmap-parsed.csv")
+        munmap_file = open("input/" + self.allocator.name + "-" + gimp_test.name + "-munmap-parsed.csv")
+        brk_file = open("input/" + self.allocator.name + "-" + gimp_test.name + "-brk-parsed.csv")
+
+        mmap_reader = csv.reader(mmap_file)
+        munmap_reader = csv.reader(munmap_file)
+        brk_reader = csv.reader(brk_file)
+        next(mmap_reader)
+        next(munmap_reader)
+        next(brk_reader)
+
+        mmap_sizes = [math.log(int(i[5])) for i in mmap_reader]
+        plt.hist(mmap_sizes, bins=100, log=True)
+
+        plt.ylabel('Count')
+        plt.xlabel('Log(Memory)')
+        plt.savefig(
+            "output/" + self.allocator.name + "-" + gimp_test.name + "-" + GraphName.STRACE_MEMORY_COUNT_HIST.name)
 
     def plot_proc_page_faults(self, gimp_test: GimpTestName):
         fault_file = open(
@@ -126,20 +182,67 @@ class Collector:
             if gimp_test != GimpTestName.UNSHARP: continue
 
             print("# COLLECTING FAULTS FOR", gimp_test.name)
-            #self.collect_faults(gimp_test)
+            # self.collect_faults(gimp_test)
 
             print("# COLLECTING MEMUSE FOR", gimp_test.name)
-            #self.collect_memory_consumption(gimp_test)
+            # self.collect_memory_consumption(gimp_test)
 
             self.collect_strace(gimp_test)
             print("# COLLECTED STRACE")
 
     def collect_strace(self, gimp_test: GimpTestName):
-        strace_path = "input/" + self.allocator.name + "-" + gimp_test.name + "-" + "strace.txt"
+        strace_path = "input/" + self.allocator.name + "-" + gimp_test.name + "-strace.txt"
         exec_shell_cmd(
             ALLOCATOR_CMD_PREFIX_MAP[allocator] + " sudo strace -T -tt -o " + strace_path + " -q -e trace=memory -f " +
             TEST_CMD_MAP[gimp_test])
 
+        strace_file = open(strace_path, "r")
+        syscall_to_args_map = {
+            "mmap": ["addr", "length", "prot", "flags", "fd", "offset"],
+            "mmap_anon": ["addr", "length", "prot", "flags", "fd", "offset"],
+            "munmap": ["addr", "length"],
+            "mprotect": ["addr", "length", "prot"],
+            "brk": ["addr"],
+            "shmdt": ["shmaddr"],
+            "shmat": ["shmid", "shmaddr", "shmflg"],
+        }
+
+        metadata_headers = ["pid", "timestamp", "ret_val", "duration"]
+        syscall_to_args_map = {key: metadata_headers + syscall_to_args_map[key] for key in syscall_to_args_map}
+        syscall_results_map = {key: [syscall_to_args_map[key]] for key in syscall_to_args_map}
+        syscall_to_stacks_map = dict()
+
+        for line in strace_file.readlines():
+            for call_to_parse in syscall_to_args_map:
+                if " " + call_to_parse + "(" in line:
+                    if "<unfinished ...>" not in line:
+                        arguments = parse_call(line, call_to_parse)
+                        syscall_results_map[call_to_parse].append(arguments)
+                        if call_to_parse == "mmap" and ("MAP_ANON" in arguments[7] or "MAP_ANONYMOUS" in arguments[7]):
+                            syscall_results_map["mmap_anon"].append(arguments)
+                    else:
+                        arguments = parse_call_for_unfinished(line, call_to_parse)
+                        if arguments[0] + call_to_parse not in syscall_to_stacks_map:
+                            syscall_to_stacks_map[arguments[0] + call_to_parse] = []
+                        syscall_to_stacks_map[arguments[0] + call_to_parse].append(arguments)
+                    break
+                elif "<... " + call_to_parse + " resumed>" in line:
+                    fields = get_fields_for_resumed(line)
+                    arguments = syscall_to_stacks_map[fields[0] + call_to_parse].pop()
+                    arguments.insert(2, fields[-2])
+                    arguments.insert(3, fields[-1])
+                    syscall_results_map[call_to_parse].append(arguments)
+                    if call_to_parse == "mmap" and ("MAP_ANON" in arguments[7] or "MAP_ANONYMOUS" in arguments[7]):
+                        syscall_results_map["mmap_anon"].append(arguments)
+                    break
+
+        # Save results of each call in their own csv
+        for call_name in syscall_to_args_map:
+            parsed_file_name = "input/" + self.allocator.name + "-" + gimp_test.name + "-" + call_name + "parsed.csv"
+            call_results_file = open(parsed_file_name, "w")
+            mmap_csv = csv.writer(call_results_file)
+            mmap_csv.writerows(syscall_results_map[call_name])
+            call_results_file.close()
 
     def collect_memory_consumption(self, gimp_test: GimpTestName):
         memory_csv_path = "input/" + self.allocator.name + "-" + gimp_test.name + "-" + GraphName.PROC_MEMORY_CONSUMPTION.name + ".csv"
@@ -187,6 +290,50 @@ class Collector:
             time.sleep(self.poll_interval)
 
         faults_csv_file.close()
+
+
+def parse_call(syscall, fn_name):
+    pid, timestamp, ret_val, duration = get_fields(syscall)
+    fn_args_list = get_fn_arguments(syscall, fn_name)
+    return [pid, timestamp, ret_val, duration] + fn_args_list
+
+
+def get_fields(syscall):
+    syscall = syscall.replace("\n", "")
+    res = syscall.split(" ")
+    return res[0], res[1], res[-2], res[-1]
+
+
+def get_fn_arguments(syscall, fn_name):
+    pattern = re.compile("(?<=" + fn_name + "\().*(?=\))")
+    fn_args_str = pattern.findall(syscall)[0]
+    fn_args_list = [i.strip() for i in fn_args_str.split(",")]
+    return fn_args_list
+
+
+def parse_call_for_unfinished(syscall, fn_name):
+    pid, timestamp = get_fields_for_unfinished(syscall)
+    fn_args_list = get_fn_arguments_for_unfinished(syscall, fn_name)
+    return [pid, timestamp] + fn_args_list
+
+
+def get_fields_for_unfinished(syscall):
+    syscall = syscall.replace("\n", "")
+    res = syscall.split(" ")
+    return res[0], res[1]
+
+
+def get_fn_arguments_for_unfinished(syscall, fn_name):
+    pattern = re.compile("(?<=" + fn_name + "\().*(?=\<)")
+    fn_args_str = pattern.findall(syscall)[0]
+    fn_args_list = [i.strip() for i in fn_args_str.split(",")]
+    return fn_args_list
+
+
+def get_fields_for_resumed(syscall):
+    syscall = syscall.replace("\n", "")
+    res = syscall.split(" ")
+    return res[0], res[-2], res[-1]
 
 
 if __name__ == "__main__":
